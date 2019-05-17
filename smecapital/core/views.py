3fe02827c37f5f8ef1831cb9210ec3cb5fa806fc
Melanie Freshness <melanie.freshness@gmail.com>
2019-05-17 15:24:17 +0100

    Adds login and templates

--- smecapital/core/views.py
+++ smecapital/core/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .forms import SignUpForm
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
+
+def home(request, start=0, end=10):
+
+    return render(request, 'home.html')
