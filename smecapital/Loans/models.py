dc04cd6f4b86c7e65900a197dc606c8fd7401874
Melanie Freshness <melanie.freshness@gmail.com>
2019-05-19 12:51:43 +0100

    Changes interest_rate_pa to interest_rate_per_month as this was wrong. Adds total amount repaid and final repayment date property methods

--- smecapital/Loans/models.py
+++ smecapital/Loans/models.py
@@ -1,3 +1,6 @@
+from decimal import Decimal
+
+from dateutil.relativedelta import relativedelta
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
@@ -16,9 +19,35 @@ class Loans(models.Model):
             MaxValueValidator(1000000),
             MinValueValidator(2000)
         ])
-    interest_rate_pa = models.DecimalField(max_digits=5, decimal_places=2)
+    interest_rate_per_month = models.DecimalField(max_digits=5, decimal_places=2)
 
     @property
     def name(self):
         name = self.borrower.name
         return name
+
+    @property
+    def APR(self):
+        APR = self.interest_rate_per_month * 12
+        return APR
+
+    @property
+    def monthly_repayment(self):
+        monthly_interest_rate = self.interest_rate_per_month / 100
+        principal = self.principal
+        months = self.term_in_months
+        monthly_repayment_numerator = (monthly_interest_rate * principal) * pow((1 + monthly_interest_rate), months)
+        monthly_repayment_denominator = pow((1 + monthly_interest_rate), months) - 1
+        monthly_repayment = monthly_repayment_numerator/monthly_repayment_denominator
+        return f"{monthly_repayment:.2f}"
+
+    @property
+    def total_amount_repaid(self):
+        total_amount_repaid = Decimal(self.monthly_repayment) * self.term_in_months
+        return f"{total_amount_repaid:.2f}"
+
+
+    @property
+    def final_repayment_date(self):
+        final_repayment_date = self.start_date + relativedelta(months=+(self.term_in_months - 1))
+        return final_repayment_date
